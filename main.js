52b791c9695f2e20eb9bfcba7991121d748487f3
Akio <cristian.akio1102@gmail.com>
2023-01-02 16:24:06 -0500

    Clase 9

--- main.js
+++ main.js
@@ -47,37 +47,3 @@ function deepCopy(subject) {
   }
   return copySubject;
 }
-
-const juan = {
-  name: "Juanito",
-  approvedCourses: ["Curso 1", "Curso 2"],
-  caracteristicas: {
-    age: 18,
-    colorCabello: "Negro",
-    gustos: {
-      musica: ["rock", "punk", "ska"],
-      peliculas: ["drama", "horros", "comedia"],
-    },
-  },
-  addCourse(newCourse) {
-    console.log("This", this);
-    console.log("This.approvedCourses", this.approvedCourses);
-    this.approvedCourses.push(newCourse);
-  },
-};
-
-function deepFreeze(obj) {
-  for (key in obj) {
-    if (typeof obj[key] === "object") {
-      deepFreeze(Object.freeze(obj[key]));
-    }
-    return Object.freeze(obj);
-  }
-}
-
-function deepFreeze2(obj) {
-  Object.keys(obj).forEach((prop) => {
-    if (typeof obj[prop] === "object") deepFreeze2(obj[prop]);
-  });
-  return Object.freeze(obj);
-}
