e66b02c01bb359c3b85fa9f65c663325988fa5d4
Akio <cristian.akio1102@gmail.com>
2023-01-02 12:01:56 -0500

    Clase 7

--- main.js
+++ main.js
@@ -5,12 +5,20 @@ const obj = {
     d: "d",
     c: "c",
   },
+  editA() {
+    this.a = "AAAA";
+  },
 };
 
-const obj2 = {};
-for (prop in obj) {
-  obj2[prop] = obj[prop];
-}
+const stringifiedComplexObj = JSON.stringify(obj);
+const obj2 = JSON.parse(stringifiedComplexObj);
+console.log({
+  stringifiedComplexObj,
+  obj2,
+});
+// for (prop in obj) {
+//   obj2[prop] = obj[prop];
+// }
 
-const obj3 = Object.assign({}, obj);
-const obj4 = Object.create(obj);
+// const obj3 = Object.assign({}, obj);
+// const obj4 = Object.create(obj);
