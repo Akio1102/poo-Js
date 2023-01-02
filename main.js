a5bb45694993a21d720127519931a3d1808e7d1e
Akio <cristian.akio1102@gmail.com>
2023-01-02 16:23:35 -0500

    Clase 9

--- main.js
+++ main.js
@@ -1,34 +1,83 @@
-// const obj = {
-//   a: "a",
-//   b: "b",
-//   c: {
-//     d: "d",
-//     c: "c",
-//   },
-//   editA() {
-//     this.a = "AAAA";
-//   },
-// };
+const obj = {
+  a: "a",
+  b: "b",
+  c: {
+    d: "d",
+    c: "c",
+  },
+  editA() {
+    this.a = "AAAA";
+  },
+};
 
-// const stringifiedComplexObj = JSON.stringify(obj);
-// const obj2 = JSON.parse(stringifiedComplexObj);
-// console.log({
-//   stringifiedComplexObj,
-//   obj2,
-// });
+function isObject(subject) {
+  return typeof subject == "object";
+}
 
-const numeritos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// let numerito = 0;
-// for (let index = 0; index < numeritos.length; index++) {
-//   numerito = numeritos[index];
-//   console.log({ index, numerito });
-// }
+function isArray(subject) {
+  return Array.isArray(subject);
+}
 
-function recursiva(arrayNums) {
-  if (arrayNums.length != 0) {
-    const firtNum = arrayNums[0];
-    console.log(firtNum);
-    arrayNums.shift();
-    recursiva(arrayNums);
+function deepCopy(subject) {
+  let copySubject;
+
+  const subjectIsObject = isObject(subject);
+  const subjectIsArray = isArray(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
   }
+
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+  return copySubject;
+}
+
+const juan = {
+  name: "Juanito",
+  approvedCourses: ["Curso 1", "Curso 2"],
+  caracteristicas: {
+    age: 18,
+    colorCabello: "Negro",
+    gustos: {
+      musica: ["rock", "punk", "ska"],
+      peliculas: ["drama", "horros", "comedia"],
+    },
+  },
+  addCourse(newCourse) {
+    console.log("This", this);
+    console.log("This.approvedCourses", this.approvedCourses);
+    this.approvedCourses.push(newCourse);
+  },
+};
+
+function deepFreeze(obj) {
+  for (key in obj) {
+    if (typeof obj[key] === "object") {
+      deepFreeze(Object.freeze(obj[key]));
+    }
+    return Object.freeze(obj);
+  }
+}
+
+function deepFreeze2(obj) {
+  Object.keys(obj).forEach((prop) => {
+    if (typeof obj[prop] === "object") deepFreeze2(obj[prop]);
+  });
+  return Object.freeze(obj);
 }
