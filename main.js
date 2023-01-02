a673fdc6c2113463201d871e1b62378d0ef68948
Akio <cristian.akio1102@gmail.com>
2023-01-02 13:56:56 -0500

    Clase 8

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-const obj = {
-  a: "a",
-  b: "b",
-  c: {
-    d: "d",
-    c: "c",
-  },
-  editA() {
-    this.a = "AAAA";
-  },
-};
+// const obj = {
+//   a: "a",
+//   b: "b",
+//   c: {
+//     d: "d",
+//     c: "c",
+//   },
+//   editA() {
+//     this.a = "AAAA";
+//   },
+// };
 
-const stringifiedComplexObj = JSON.stringify(obj);
-const obj2 = JSON.parse(stringifiedComplexObj);
-console.log({
-  stringifiedComplexObj,
-  obj2,
-});
-// for (prop in obj) {
-//   obj2[prop] = obj[prop];
+// const stringifiedComplexObj = JSON.stringify(obj);
+// const obj2 = JSON.parse(stringifiedComplexObj);
+// console.log({
+//   stringifiedComplexObj,
+//   obj2,
+// });
+
+const numeritos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let numerito = 0;
+// for (let index = 0; index < numeritos.length; index++) {
+//   numerito = numeritos[index];
+//   console.log({ index, numerito });
 // }
 
-// const obj3 = Object.assign({}, obj);
-// const obj4 = Object.create(obj);
+function recursiva(arrayNums) {
+  if (arrayNums.length != 0) {
+    const firtNum = arrayNums[0];
+    console.log(firtNum);
+    arrayNums.shift();
+    recursiva(arrayNums);
+  }
+}
